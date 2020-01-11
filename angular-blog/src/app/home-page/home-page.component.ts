1974ac3fa0600654b34ffcbbb0f8618f054679de
RudolfKlikh <rudolfklikh@gmail.com>
2020-01-11 19:07:06 +0100

    End

--- angular-blog/src/app/home-page/home-page.component.ts
+++ angular-blog/src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../shared/posts.service';
+import { Observable } from 'rxjs';
+import { Post } from '../admin/shared/interfaces/interface';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  posts$: Observable<Post[]>;
+  constructor(private postService: PostsService) { }
 
   ngOnInit() {
+    this.posts$ = this.postService.getAll();
   }
 
 }
--- angular-blog/src/app/shared/components/post/post.component.ts
+++ angular-blog/src/app/shared/components/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/admin/shared/interfaces/interface';
 
 @Component({
   selector: 'app-post',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class PostComponent implements OnInit {
 
   constructor() { }
+
+  @Input() post: Post;
 
   ngOnInit() {
   }
